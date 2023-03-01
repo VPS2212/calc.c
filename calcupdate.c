8ed6f08f69a039ea46af17a3607014cec5f7e415
VPS2212 <126179069+VPS2212@users.noreply.github.com>
2023-03-01 20:54:04 +0530

    calc code updated file

---
+++ calcupdate.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+int a,b,opt;
+float res;
+printf("Enter the first Number:\n");
+scanf("%d",&a);
+printf("Enter the second Number:\n");
+scanf("%d",&b);
+printf("Select the select calucation mode \n 1-ADD \n 2-Sub \n 3-Multiplication \n 4-Division \n");
+scanf("%d",&opt);
+switch (opt)
+{
+case 1:
+    res=a+b;
+    printf("%f\n",res);
+    break;
+case 2:
+    res=a-b;
+    printf("%f\n",res);
+    break;
+case 3:
+    res=a*b;
+    printf("%f\n",res);
+    break;
+case 4:
+    res=a/b;
+    printf("%f\n",res);
+    break;
+default:
+printf("somthing went wrong\n");
+}
+printf("\n ************************************************************** \n");
+return 0;
+}
+
