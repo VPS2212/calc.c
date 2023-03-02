63e7edc8db7d5ceca659b02f4d19391a83e14cd7
VPS2212 <126179069+VPS2212@users.noreply.github.com>
2023-03-02 23:07:46 +0530

    Add files via upload
    mode selection statement is replaced

--- calcupdate.c
+++ calcupdate.c
@@ -1,22 +1,24 @@
-:#include<stdio.h>
+#include<stdio.h>
 int main()
 {
-int a,b,opt=0;
+int a,b,opt=1;
 float res;
-printf("Enter the first Number:\n");
-scanf("%d",&a);
 printf("Select the select calucation mode \n 1-ADD \n 2-Sub \n 3-Multiplication \n 4-Division \n");
 scanf("%d",&opt);
-if (opt<=4)
+if (opt<4)
 {
-printf("Enter the second Number:\n");
-scanf("%d",&b);
+       if(opt<=0)
+       {	
+	printf("Select the proper Mode");
+       }
+       else
+       {       
+	printf("Enter the first Number:\n");
+	scanf("%d",&a);
+	printf("Enter the second Number:\n");
+	scanf("%d",&b);
+       }
 }
-else
-{
-	printf("Select The Proper Mode");
-}
-
 switch (opt)
 {
 case 1:
